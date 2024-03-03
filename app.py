50cc73ada169cdc681f0ce1b0592dcafda7f2936
karan-88 <127552754+karan-88@users.noreply.github.com>
2024-03-03 18:54:31 -0500

    Update app.py
    edited predict function

--- app.py
+++ app.py
@@ -36,24 +36,14 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    num_columns = 5
+    cols = st.columns(num_columns)
+    for i in range(num_columns):
+        with cols[i]:
+            if i < len(recommended_movie_names):
+                st.text(recommended_movie_names[i])
+                st.image(recommended_movie_posters[i])
 
 
 
